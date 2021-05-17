1913230d62c9782abc82a89ee409174ea50ebe84
Edson Rosales <edsonme9@gmail.com>
2021-05-17 12:19:59 -0500

    The construction of paginate the url of each pokemon

--- app/hooks/usePokemonPaginated.tsx
+++ app/hooks/usePokemonPaginated.tsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { pokemonApi } from "../api/pokemonApi";
+import { PokemonPaginatedResponse, Result, SimplePokemon } from "../interfaces/PokemonInterfaces";
 
 
 export const usePokemonPaginated = () => {
-
-    const url = 'https://pokeapi.co/api/v2/pokemon?limit=40';
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
+    const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
     const loadPokemons = async () => {
-        const result = await pokemonApi.get(url);
-        console.log(result);
+        const result = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+        nextPageUrl.current = result.data.next;
+        mapPokemonList(result.data.results);
+    }
+
+    //This function helps me to transform the data for save it in my state simplePokemonList
+    const mapPokemonList = (pokemonList: Result[]) => {
+        pokemonList.forEach( poke => console.log(poke.name));
     }
 
     useEffect(() => {
